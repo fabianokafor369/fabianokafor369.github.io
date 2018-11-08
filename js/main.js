7ee85762a00e28ada3da268c9476162c4cd905e4
Fabian Okafor <fabianokafor369@gmail.com>
2018-11-08 19:34:52 +0100

    m

--- js/main.js
+++ js/main.js
@@ -217,27 +217,21 @@ $(document).ready(function($) {
       newCircle.setAttribute('cx', xCords[i]);
       newCircle.setAttribute('cy', yCords[i]);
       newCircle.setAttribute('r', '1');
-      if(i === 2867 || i === 1700  || i === 4837 || i === 5016 || i === 5082 || i === 312 ) {
+      if(i === 994 || i === 1443 || i === 1604 || i === 5016 ) {
         var areaTitle;
       	newCircle.setAttribute('fill', '#c3d833');
         switch(i) {
-          case 2867:
-            areaTitle = 'Lagos,Nigeria;
+          case 994:
+            areaTitle = 'Markham, Ontario';
             break;
-          case 1700:
-            areaTitle = 'Berlin, Germany';
+          case 1443:
+            areaTitle = 'London, Storbritannien';
             break;
-          case 4837:
-            areaTitle = 'Hyderabad, India';
+          case 1604:
+            areaTitle = 'Göteborg, Sverige';
             break;
           case 5016:
             areaTitle = 'Thailand';
-            break;
-          case 5082:
-            areaTitle = 'South Korea';
-            break;
-          case 312:
-            areaTitle = 'San Francisco, United States';
             break;
         }
         newCircle.setAttribute('title', ''+areaTitle+'');
